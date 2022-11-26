140b2c7e14559243ef6301c6867282dd17d48dc1
imskanand <the.anand3099@gmail.com>
2022-11-26 19:55:04 +0530

    Reducer Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useReducer } from 'react';
 
+// ~ Reducer Function in JavaScript
+// const list = [12, 23, 45];
+// const total = list.reduce((curr, n) => curr + n);
+// console.log(total);
+
 function App() {
 	return <div></div>;
 }
